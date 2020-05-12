d635e83e765133d4e150b71ff0c2627a98c5b674
Bijan Fakhri <bfakhri@asu.edu>
2020-05-12 10:19:24 -0700

    U

--- generate_video.py
+++ generate_video.py
@@ -14,7 +14,7 @@ capture_dir = 'test/'
 path = base_dir + capture_dir
 fps = 30
 #skip_mult = 18
-skip_mult = 1
+skip_mult = 8
 
 frame_files = [f for f in listdir(path) if isfile(join(path, f))]
 frame_files.sort()
@@ -24,7 +24,7 @@ frame_files = frame_files[::skip_mult]
 frame_0 = cv2.imread(base_dir+capture_dir+frame_files[0])
 frame_width = int(frame_0.shape[1])
 frame_height = int(frame_0.shape[0])
-out = cv2.VideoWriter(base_dir+'outpy3.avi',cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width,frame_height))
+out = cv2.VideoWriter(base_dir+'outpy8.avi',cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width,frame_height))
 
 for idx,frame_f in enumerate(frame_files):
     frame = cv2.imread(base_dir+capture_dir+frame_f)
