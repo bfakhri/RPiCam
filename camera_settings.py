1ea9f54db8449cdebc7d5463da6349b548ff3cb0
Bijan Fakhri <bijan.fakhri@statsperform.com>
2021-08-16 21:46:25 -0500

    Init

---
+++ camera_settings.py
@@ -0,0 +1,4 @@
+# YOU CAN CONTROL WEBCAM SETTINGS WITH THE FOLLOWING COMMANDS:
+# list all available commands: v4l2-ctl -l
+# disable autofocus: v4l2-ctl -c focus_auto=0
+# other settings can be set manually, like focus distance etc
