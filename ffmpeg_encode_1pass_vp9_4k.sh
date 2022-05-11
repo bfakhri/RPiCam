daca7ef6acc7444923c96fe0d0b317358d01849c
Bijan Fakhri <bijan.fakhri@statsperform.com>
2022-05-11 00:43:12 -0500

    Added ref

--- ffmpeg_encode_1pass_vp9_4k.sh
+++ ffmpeg_encode_1pass_vp9_4k.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Encodes at 4k, 24fps, 56Mbit/s
+# Reference: https://trac.ffmpeg.org/wiki/Encode/VP9
 ffmpeg -pattern_type glob -i 'temper/*.png' -vf scale=3840:2160 -c:v libvpx-vp9 -r 24 -b:v 56M output.mp4
--- ffmpeg_encode_2pass_vp9_4k.sh
+++ ffmpeg_encode_2pass_vp9_4k.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Encodes at 4k, 24fps, 56Mbit/s
+# Reference: https://trac.ffmpeg.org/wiki/Encode/VP9
 ffmpeg -pattern_type glob -i 'temper/*.png' -c:v libvpx-vp9 -r 24 -vf scale=3840:2160 -b:v 56M -pass 1 -an -f null /dev/null && \
 ffmpeg -pattern_type glob -i 'temper/*.png' -c:v libvpx-vp9 -r 24 -vf scale=3840:2160 -b:v 56M -pass 2 -c:a libopus 4k_2pass_output.mp4
