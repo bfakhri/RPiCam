33c4ce3bdbfb7b2a692d9b8584cc5aabe47842ff
Bijan Fakhri <bijan.fakhri@statsperform.com>
2022-06-13 20:40:15 -0500

    Working NVIDIA encoder

--- ffmpeg_encode_1pass_vp9_4k.sh
+++ ffmpeg_encode_1pass_vp9_4k.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Encodes at 4k, 24fps, 56Mbit/s
-# Reference: https://trac.ffmpeg.org/wiki/Encode/VP9
-ffmpeg -pattern_type glob -i 'temper/*.png' -vf scale=3840:2160 -c:v libvpx-vp9 -r 24 -b:v 56M output.mp4
+#ffmpeg -pattern_type glob -i './fast/*.png' -c:v h264_nvenc -vf scale=1920:1080 -r 24 -b:v 10M output_fast_smaller.avi
+ffmpeg -pattern_type glob -i './fast/*.png' -c:v h264_nvenc -r 24 -b:v 100M output_fast.mp4
